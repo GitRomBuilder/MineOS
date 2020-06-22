5156973a38e40478d2e581eb9d1ef905f32a338b
GitRomBuilder <57009359+GitRomBuilder@users.noreply.github.com>
2020-06-21 21:54:41 -0400

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -2,6 +2,7 @@
 --Import Libraries
 local GUI = require("GUI")
 local system = require("System")
+local internet = require("Internet")
 
 --Restore files
 internet.download("https://raw.githubusercontent.com/GitRomBuilder/ShadowOS/master/Applications/MineCode%20IDE.app/Localizations/English.lang", "Applications/MineCode IDE/Localizations")
