7cbfecd2e4cc77b45af81de2061107413b6410ae
GitRomBuilder <57009359+GitRomBuilder@users.noreply.github.com>
2020-06-21 22:03:35 -0400

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -5,4 +5,4 @@ local system = require("System")
 local internet = require("Internet")
 
 --Restore files
-internet.download("https://raw.githubusercontent.com/GitRomBuilder/ShadowOS/master/Applications/MineCode%20IDE.app/Localizations/English.lang", "Applications/MineCode IDE/Localizations")
+internet.download("https://raw.githubusercontent.com/GitRomBuilder/ShadowOS/master/Applications/MineCode%20IDE.app/Localizations/English.lang", "Applications/MineCode IDE.app/Localizations/English.lang")
